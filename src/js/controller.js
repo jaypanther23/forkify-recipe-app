4e2812c6e644bbb0c8c09b12de403c0a32f7f0f0
jaypanther23 <davisjal96@gmail.com>
2022-09-27 16:42:59 -0500

    initial commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -136,7 +136,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  console.log('Awesome');
 };
 
 init();
