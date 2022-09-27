a7fbab511fbd5a4ba6fb4c28ab344b9106a8fc08
Jalen Davis <98918133+jaypanther23@users.noreply.github.com>
2022-09-27 09:47:22 -0500

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+// all the variables that should be re-used
+//defines important data about the application
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+//common practice to use upper case for constant
+export const KEY = 'f938df61-55cb-4077-8e23-0b69881d9c66';
+export const MODAL_CLOSE_SEC = 2.5;
